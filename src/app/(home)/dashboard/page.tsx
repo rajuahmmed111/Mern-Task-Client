e54362d91c7c5cfb524153852deb9262e074ff76
Raju Ahmmed <ahraju2003@gmail.com>
2025-05-04 18:01:07 +0600

    update dashboard page

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -1,14 +1,11 @@
-// import DashboardContainer from "@/components/dashboard/DashboardContainer";
-// import DashboardHeader from "@/components/dashboard/DashboardHeader";
-
-import TaskDetails from "./task-details/[id]/page";
+import DashboardContainer from "@/components/dashboard/DashboardContainer";
+import DashboardHeader from "@/components/dashboard/DashboardHeader";
 
 export default function Component() {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* <DashboardHeader />
-      <DashboardContainer /> */}
-      <TaskDetails />
+      <DashboardHeader />
+      <DashboardContainer />
     </div>
   );
 }
